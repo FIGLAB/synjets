69d35f2024aba9527b8a43a9023eb9cb7d4ff284
vhshen <vivianhshen@gmail.com>
2023-05-04 13:54:30 -0400

    adding unity project for the HMD demos

---
+++ synjet_HMD_demos/Assets/Scripts/start_splash.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start_splash : MonoBehaviour
+{
+    public ParticleSystem system;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        system.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(system);
+        system.Pause();
+    }
+}
